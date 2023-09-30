d1b83befa12f808454a04232dbe8da593bafe493
KantyRas <rasolofomananakanty@gmail.com>
2023-09-30 17:02:05 +0300

    kanty

---
+++ application/controllers/LogController.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class LogController extends CI_Controller {
+
+	public function disconnect(){
+        $this->session->sess_destroy();
+        redirect("Welcome/index");
+    }
+    public function loadViewAccueil($data){
+        $this->load->view('accueil',$data);
+		$this->load->view('content');
+		$this->load->view('footer');
+    }
+	public function getLogin()
+	{
+		$email = $this->input->post("email");
+        $password = $this->input->post("password");
+        $this->load->model('SessionModel');
+        $bool = $this->SessionModel->checkUser($email, $password);
+        if($bool){
+            $data = $_SESSION['huhu'];
+            /*if($data['estadmin'] != 10) $this->loadAccueil($data);
+            if($data['estadmin'] == 10) $this->loadAdminSpace($data);*/
+            $this->loadViewAccueil($data);
+        }
+        else if(!$bool){
+            redirect('Welcome/index');                          
+        }
+	}		
+}
---
+++ application/models/SessionModel.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class utils_model extends CI_Model {
+
+	public function checkUser($email, $password)
+	{
+		$query = $this->db->query("select * from testa where email='$email' and mdp='$password'");
+		$row = $query->row_array();
+		if($row){
+			$this->load->library('session');
+			$this->session;
+			$this->session->set_userdata('huhu', $row);
+			return true;
+		}
+		return false;
+	}		
+}
